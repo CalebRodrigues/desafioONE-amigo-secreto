4a22a44e8fd3b120152108c14a213dc2fde2b1f5
Caleb Rodrigues <c4l3b23@gmail.com>
2025-01-17 16:53:27 -0300

    funcionalidade atualizar lista

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 let amigos = [];
 
+function atualizarLista() {
+    let elementosLista = "";
+    for (i = 0; i < amigos.length; i++) {
+        elementosLista += "<li>" + amigos[i] + "</li>";
+    }
+    document.getElementById("listaAmigos").innerHTML = elementosLista;
+}
+
 function adicionarAmigo() {
     let nomeAmigo = document.getElementById("amigo").value;
     if (nomeAmigo != "") {
         document.getElementById("amigo").value = "";
         amigos.push(nomeAmigo);
+        atualizarLista();
     } else {
         alert("Por favor, insira um nome.");
     }
