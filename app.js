0630b80ebec38547423a09bce5a9bbd17ab10c84
Caleb Rodrigues <c4l3b23@gmail.com>
2025-01-17 16:14:55 -0300

    funcionalidade adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo").value;
+    if (nomeAmigo != "") {
+        document.getElementById("amigo").value = "";
+        amigos.push(nomeAmigo);
+    } else {
+        alert("Por favor, insira um nome.");
+    }
+}
